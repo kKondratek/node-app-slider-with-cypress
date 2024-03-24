77875f1de97ae4258bcbfc53abf8cf323a09aba9
kkondratek <kkondratek@guidewire.com>
2024-03-24 09:51:46 +0100

    Add test and hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,18 +1,33 @@
 describe('Swiper Gallery Test', function () {
-  it('Checks if second slide contains "United Kingdom"', function () {
+  it('Checks if second slide contains "London" and "United Kingdom""', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
+    cy.get('.swiper-slide-active').should('contain', 'London');
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
 });
 
 describe('Swiper Gallery Test', function () {
-  it('Checks if third slide contains "Paris"', function () {
+  it('Checks if third slide contains "Paris" and "France"', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000);
     cy.get('.swiper-button-next').click({ force: true });
     cy.wait(2000);
     cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('contain', 'France');
+  });
+});
+
+describe('Swiper Gallery Test with Keyboard Arrows', function () {
+  it('Checks if third slide contains "Paris" and "France" using keyboard arrows', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('body').click();
+    cy.get('body').type('{rightarrow}');
+    cy.wait(2000);
+    cy.get('body').type('{rightarrow}');
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('contain', 'France');
   });
 });
